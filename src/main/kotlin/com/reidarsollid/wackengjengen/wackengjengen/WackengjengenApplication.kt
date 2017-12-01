9669ea13204b10147359592cbcca5a4f90051551
Reidar Sollid <rsollid@gmail.com>
2017-12-01 16:35:15 +0100

    Spring INITIALIZR

---
+++ src/main/kotlin/com/reidarsollid/wackengjengen/wackengjengen/WackengjengenApplication.kt
@@ -0,0 +1,11 @@
+package com.reidarsollid.wackengjengen.wackengjengen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WackengjengenApplication
+
+fun main(args: Array<String>) {
+    runApplication<WackengjengenApplication>(*args)
+}
